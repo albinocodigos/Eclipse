0e5720ba99c7a17a6032beac09b09b8af6d72797
Professor : Bino <116452130+albinocodigos@users.noreply.github.com>
2023-02-07 00:12:58 -0300

    Add files via upload

---
+++ ProfJoseAssis/src/eclipseComGithub/ProjetoGithub.java
@@ -0,0 +1,15 @@
+package eclipseComGithub;
+
+// Exemplo de interação do Eclipse com Github.
+
+// Aula: Professor José de Assis.
+
+public class ProjetoGithub {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Bem vindo ao GitHub em 06.02.2023");
+
+	}
+
+}
