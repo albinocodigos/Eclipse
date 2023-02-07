ed4d0c9cf634bf60e00f22d72848d5716f9e0b54
Albino Leal <sociedadejava@gmail.com>
2023-02-07 16:26:21 -0300

    Adicionei duas linhas no projeto!

--- ProfJoseAssis/src/eclipseComGithub/ProjetoGithub.java
+++ ProfJoseAssis/src/eclipseComGithub/ProjetoGithub.java
@@ -9,6 +9,10 @@ public class ProjetoGithub {
 	public static void main(String[] args) {
 		
 		System.out.println("Bem vindo ao GitHub em 06.02.2023");
+		
+		System.out.println("Fazendo alterações no projeto e enviando ao Github!");
+		
+		System.out.println("Data da alteração: 07.02.2023");
 
 	}
 
